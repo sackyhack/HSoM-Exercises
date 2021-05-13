ee5f3118cbe3a06c34beef4d3ba435e316e1c8e5
Craig Ferguson <craig@tworing.ca>
2021-05-13 13:45:10 -0700

    Add 9.3

--- src/Ch9.hs
+++ src/Ch9.hs
@@ -2,6 +2,8 @@ module Ch9 where
 import Euterpea
 import HSoM
 
+import Data.List(sort)
+
 newPlayer :: Player (Pitch, [NoteAttribute])
 newPlayer = defPlayer {
     pName = "NewPlayer",
@@ -21,6 +23,7 @@ myPasHandler pa pf = defPasHandler pa pf
 
 myPMap :: PlayerName -> Player Note1 
 myPMap "NewPlayer" = newPlayer
+myPMap "MyPlayer" = myPlayer
 myPMap p = defPMap p
 
 scaleCon = defCon {
@@ -34,3 +37,39 @@ scaleCrescendo x = Modify (Custom "Player NewPlayer") $ Modify (Phrase [Dyn (Cre
 scaleStaccato x = Modify (Phrase [Art (Staccato x)]) scale
 
 scaleCrescendoTest x = playA myPMap scaleCon (scaleCrescendo x)
+
+myPlayer = defPlayer {
+    pName = "MyPlayer",
+    interpPhrase = myInterpPhrase
+}
+
+myInterpPhrase :: PhraseFun a 
+myInterpPhrase pm c [] m = perf pm c m
+myInterpPhrase pm 
+    c@Context { cTime = t, cPlayer = pl, cInst = i,
+        cDur = dt, cPch = k, cVol = v }
+    (pa:pas) m =
+        let pfd@(pf, dur) = myInterpPhrase pm c pas m
+            aDelay = cDur c / 20
+            first = head pf
+        in case pa of 
+            Art Pedal -> -- Extend duration of each event to the end of the phrase
+                let t0 = eTime (head pf)
+                    upd e@MEvent {eTime = t} = e {eDur = dur - (t - t0)} 
+                in (map upd pf, dur)
+            Orn ArpeggioUp ->
+                let (arp, rest) = span (\e -> eTime e == eTime first && eInst e == eInst first) pf
+                    arp' = map (\(n, e@MEvent {eTime = t, eDur = d}) -> e {eTime = t + aDelay * n, eDur = d - aDelay * n}) $ zip [0..] (sort arp)
+                in (merge arp' rest, dur)
+            Orn ArpeggioDown -> 
+                let (arp, rest) = span (\e -> eTime e == eTime first && eInst e == eInst first) pf
+                    arp' = map (\(n, e@MEvent {eTime = t, eDur = d}) -> e {eTime = t + aDelay * n, eDur = d - aDelay * n}) $ zip [0..] (reverse (sort arp))
+                in (merge arp' rest, dur)
+            _ -> pfd
+
+pedalTest = playA myPMap scaleCon $ Modify (Custom "Player MyPlayer") $ Modify (Phrase [Art Pedal]) scale
+
+arpeggio = (c 4 qn :=: e 4 qn :=: g 4 qn) :+: d 4 qn :+: e 4 qn :+: f 4 qn :+: g 4 qn
+arpeggio2 = (g 4 qn :=: e 4 qn :=: c 4 qn) :+: d 4 qn :+: e 4 qn :+: f 4 qn :+: g 4 qn
+arpUpTest = playA myPMap scaleCon $ Modify (Custom "Player MyPlayer") $ Modify (Phrase [Orn ArpeggioUp]) arpeggio
+arpDownTest = playA myPMap scaleCon $ Modify (Custom "Player MyPlayer") $ Modify (Phrase [Orn ArpeggioDown]) arpeggio
